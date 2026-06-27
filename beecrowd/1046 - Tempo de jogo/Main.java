a35babbb251fa3c1c543a0830043696a16e1413d
Lucas Godoy <lucasgodoyjor@gmail.com>
2026-06-27 17:52:18 -0300

    Adiciona ex. 1046 - Tempo de jogo

---
+++ beecrowd/1046 - Tempo de jogo/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int inicio = scanner.nextInt();
+        int fim = scanner.nextInt();
+        scanner.close();
+
+        
+        int duracao;
+
+        if (fim>inicio){
+            duracao = fim - inicio;
+        } else {
+            
+            duracao = (24 - inicio) + fim;
+        }
+        System.out.printf("O JOGO DUROU %d HORA(S)%n",duracao);
+        
+    }
+
+}

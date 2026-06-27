710d163f5ac7f5e0324c46d8342028f584f17762
Lucas Godoy <lucasgodoyjor@gmail.com>
2026-06-27 18:03:27 -0300

    Adiciona ex. 1047 - Tempo de jogo com minutos

--- beecrowd/1046 - Tempo de jogo/Main.java
+++ beecrowd/1046 - Tempo de jogo/Main.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class Main {
     
+    @SuppressWarnings("ConvertToTryWithResources")
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int inicio = scanner.nextInt();
---
+++ beecrowd/1047 - Tempo de jogo com minutos/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+
+    @SuppressWarnings("ConvertToTryWithResources")
+    public static void main(String[] args) {
+    
+        Scanner scanner = new Scanner(System.in);
+        int horaInicio = scanner.nextInt();
+        int minutosInicio = scanner.nextInt();
+        int horaFim = scanner.nextInt();
+        int minutosFim = scanner.nextInt();
+        scanner.close();
+        
+        minutosInicio=horaInicio*60+minutosInicio;
+
+        minutosFim=horaFim*60+minutosFim;
+
+        int duracao;
+
+        if (minutosFim>minutosInicio){
+            duracao = minutosFim - minutosInicio;
+        } else {
+            
+            duracao = (1440 - minutosInicio) + minutosFim;
+        }
+
+        int duracaoHoras = duracao/60;
+        duracao = duracao%60;
+
+        System.out.printf("O JOGO DUROU %d HORA(S) E %d MINUTO(S)%n",duracaoHoras,duracao);
+        
+    }
+    
+}

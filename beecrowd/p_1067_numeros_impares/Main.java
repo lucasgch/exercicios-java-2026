b33e51dfd2c030a43367ef7125c38a42461b14d9
Lucas <Lucasgodoyjor@gmail.com>
2026-06-30 10:16:30 -0300

    Adiciona ex. 1066 pares impares positivos negativos

---
+++ beecrowd/p_1067_numeros_impares/Main.java
@@ -0,0 +1,22 @@
+package beecrowd.p_1067_numeros_impares;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    @SuppressWarnings("ConvertToTryWithResources")
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+        int numero = sc.nextInt();
+        sc.close();
+        for (int i = 1; i <= numero; i++) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+
+    }
+
+}

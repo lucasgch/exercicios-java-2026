e1a5a0070062023fb6f8987766d56abdb2b949af
Lucas <Lucasgodoyjor@gmail.com>
2026-07-03 13:33:06 -0300

    Adiciona ex. 1101

--- beecrowd/iniciante/p3/p1101_seq_nums_e_soma/Main.java
+++ beecrowd/iniciante/p3/p1101_seq_nums_e_soma/Main.java
@@ -1,23 +1,37 @@
 package beecrowd.iniciante.p3.p1101_seq_nums_e_soma;
 
 import java.io.IOException;
+import java.util.Scanner;
 
-/**
- * IMPORTANT:
- *      O nome da classe deve ser "Main" para que a sua solução execute
- *      Class name must be "Main" for your solution to execute
- *      El nombre de la clase debe ser "Main" para que su solución ejecutar
- */
 public class Main {
 
     public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
 
-        /**
-         * Escreva a sua solução aqui
-         * Code your solution here
-         * Escriba su solución aquí
-         */
+        int m = sc.nextInt();
+        int n = sc.nextInt();
 
+        while (m > 0 && n > 0) {
+            int sum = 0;
+
+            // Garante que 'm' será menor
+            if (m > n) {
+                int temp = n;
+                n = m;
+                m = temp;
+            }
+
+            // Imprime a sequência e calcula a soma
+            for (int i = m; i <= n; i++) {
+                System.out.printf("%d ", i);
+                sum += i;
+            }
+            System.out.printf("Sum=%d%n", sum);
+
+            m = sc.nextInt();
+            n = sc.nextInt();
+        }
+
+        sc.close();
     }
-
 }

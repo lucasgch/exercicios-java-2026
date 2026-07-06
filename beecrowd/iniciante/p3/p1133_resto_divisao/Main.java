de41b5661c038ac33397767c951812a8093ba563
Lucas <Lucasgodoyjor@gmail.com>
2026-07-06 09:56:53 -0300

    adiciona ex. 1133

--- beecrowd/iniciante/p3/p1133_resto_divisao/Main.java
+++ beecrowd/iniciante/p3/p1133_resto_divisao/Main.java
@@ -1,4 +1,31 @@
 package beecrowd.iniciante.p3.p1133_resto_divisao;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+        int n1 =  sc.nextInt();
+        int n2 = sc.nextInt();
+        sc.close();
+        int temp = 0;
+
+        // garante que n1 é menor ou igual
+        if (n1>n2){
+            temp = n1;
+            n1 = n2;
+            n2 = temp;
+        }
+
+        for (int i = n1+1; i < n2; i++) {
+            if ( i % 5 == 2 || i % 5 == 3 ) {
+                System.out.println(i);
+            }
+        }
+
+    }
+
 }

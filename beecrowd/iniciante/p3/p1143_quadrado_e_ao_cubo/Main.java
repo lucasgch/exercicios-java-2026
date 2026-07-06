4a479b449b8f665c45373e58312de3a49896f8fb
Lucas <Lucasgodoyjor@gmail.com>
2026-07-06 10:47:08 -0300

    Adiciona ex. 1143

--- beecrowd/iniciante/p3/p1143_quadrado_e_ao_cubo/Main.java
+++ beecrowd/iniciante/p3/p1143_quadrado_e_ao_cubo/Main.java
@@ -1,4 +1,24 @@
 package beecrowd.iniciante.p3.p1143_quadrado_e_ao_cubo;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            // Calcula o quadrado e o cubo
+            int quadrado = i * i;
+            int cubo = i * i * i;
+
+            // Imprime com espaços entre, mas SEM espaço depois do cubo
+            System.out.printf("%d %d %d\n", i, quadrado, cubo);
+        }
+
+        sc.close();
+    }
 }

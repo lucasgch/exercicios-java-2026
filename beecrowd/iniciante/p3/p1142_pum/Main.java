60aa0349ea468e346b60f70c3ba947ebfc39222f
Lucas <Lucasgodoyjor@gmail.com>
2026-07-06 10:29:01 -0300

    Adiciona ex. 1142

--- beecrowd/iniciante/p3/p1142_pum/Main.java
+++ beecrowd/iniciante/p3/p1142_pum/Main.java
@@ -1,4 +1,24 @@
 package beecrowd.iniciante.p3.p1142_pum;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+        int n =  sc.nextInt();
+        int np = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= 3; j++) {
+                System.out.print(np+" ");
+                np++;
+            }
+            np++;
+            System.out.println("PUM");
+        }
+
+    }
+
 }

a24cbb1921a365b46efa5cbdb39cd35d73e82b73
Lucas <Lucasgodoyjor@gmail.com>
2026-07-06 11:05:49 -0300

    Adiciona ex. 1144

--- beecrowd/iniciante/p3/p1144_sequencia_logica/Main.java
+++ beecrowd/iniciante/p3/p1144_sequencia_logica/Main.java
@@ -1,4 +1,32 @@
 package beecrowd.iniciante.p3.p1144_sequencia_logica;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int out = 1;
+        int out2 = 1;
+        int out3 = 1;
+        for  (int i = 1; i <= n*2; i++) {
+            System.out.printf("%d %d %d%n",out,out2,out3);
+
+            if (i % 2 == 0) {
+                out++;
+                out2=(int)Math.pow(out,2);
+                out3=(int)Math.pow(out,3);
+            } else {
+                out2++;
+                out3++;
+            }
+        }
+
+        sc.close();
+
+    }
+
 }

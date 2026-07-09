69eef0e2481a6d3070b5e39735d920b57908a0d8
Lucas <Lucasgodoyjor@gmail.com>
2026-07-09 15:09:52 -0300

    Add fibo ex.

--- beecrowd/iniciante/p4/p1151_fibonacci_facil/Main.java
+++ beecrowd/iniciante/p4/p1151_fibonacci_facil/Main.java
@@ -1,23 +1,30 @@
 package beecrowd.iniciante.p4.p1151_fibonacci_facil;
 
 import java.io.IOException;
+import java.util.Scanner;
 
-/**
- * IMPORTANT:
- *      O nome da classe deve ser "Main" para que a sua solução execute
- *      Class name must be "Main" for your solution to execute
- *      El nombre de la clase debe ser "Main" para que su solución ejecutar
- */
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        /**
-         * Escreva a sua solução aqui
-         * Code your solution here
-         * Escriba su solución aquí
-         */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
 
+        int n1 = 0;
+        int n2 = 1;
+
+        for (int i = 1; i <= n; i++) {
+            if (i == n) {
+                System.out.print(n1);
+            } else {
+                System.out.print(n1 + " ");
+            }
+
+            int next = n1 + n2;
+            n1 = n2;
+            n2 = next;
+        }
+
+        System.out.println();
     }
-
 }

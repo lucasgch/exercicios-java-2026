c2c9f40d0c0ae0ccf2dc387f7e5cfa717cd2aee8
Lucas <Lucasgodoyjor@gmail.com>
2026-07-09 09:39:33 -0300

    Adiciona ex. 1149

--- beecrowd/iniciante/p4/p1149_somando_inteiros_consecutivos/Main.java
+++ beecrowd/iniciante/p4/p1149_somando_inteiros_consecutivos/Main.java
@@ -1,23 +1,29 @@
 package beecrowd.iniciante.p4.p1149_somando_inteiros_consecutivos;
 
 import java.io.IOException;
+import java.util.Scanner;
 
-/**
- * IMPORTANT:
- *      O nome da classe deve ser "Main" para que a sua solução execute
- *      Class name must be "Main" for your solution to execute
- *      El nombre de la clase debe ser "Main" para que su solución ejecutar
- */
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        /**
-         * Escreva a sua solução aqui
-         * Code your solution here
-         * Escriba su solución aquí
-         */
+        Scanner sc = new Scanner(System.in);
 
+        int A = sc.nextInt();
+        int N = sc.nextInt();
+
+        // 1. Lê até obter um N válido
+        while (N <= 0) {
+            N = sc.nextInt();
+        }
+
+        int soma = 0;
+
+        for (int i = 0; i < N; i++) {
+            soma += A + i;
+        }
+
+        System.out.println(soma);
+        sc.close();
     }
-
 }

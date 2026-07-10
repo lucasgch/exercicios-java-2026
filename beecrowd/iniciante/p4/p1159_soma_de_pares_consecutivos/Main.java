29758ccb2d65e4e17c852864fe0d271529d71618
Lucas <Lucasgodoyjor@gmail.com>
2026-07-10 14:18:26 -0300

    Adiciona ex. 1159

--- beecrowd/iniciante/p4/p1159_soma_de_pares_consecutivos/Main.java
+++ beecrowd/iniciante/p4/p1159_soma_de_pares_consecutivos/Main.java
@@ -1,23 +1,31 @@
 package beecrowd.iniciante.p4.p1159_soma_de_pares_consecutivos;
 
 import java.io.IOException;
+import java.util.Scanner;
 
-/**
- * IMPORTANT:
- *      O nome da classe deve ser "Main" para que a sua solução execute
- *      Class name must be "Main" for your solution to execute
- *      El nombre de la clase debe ser "Main" para que su solución ejecutar
- */
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        /**
-         * Escreva a sua solução aqui
-         * Code your solution here
-         * Escriba su solución aquí
-         */
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
 
+        while (x!=0){
+
+            if (x%2!=0){
+                x++;
+            }
+            int cont = 0;
+            int soma = 0;
+            for (int i = x; cont<5; i+=2) {
+                soma+=i;
+                cont++;
+            }
+            System.out.println(soma);
+            x = sc.nextInt();
+        }
+
+        sc.close();
     }
 
 }

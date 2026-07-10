f64b0f0691a002b82fc5dcd88b04f9eb7cff310d
Lucas <Lucasgodoyjor@gmail.com>
2026-07-10 13:38:35 -0300

    Adiciona ex. 1157 - divisores

--- beecrowd/iniciante/p4/p1157_divisores_i/Main.java
+++ beecrowd/iniciante/p4/p1157_divisores_i/Main.java
@@ -1,22 +1,20 @@
 package beecrowd.iniciante.p4.p1157_divisores_i;
 
 import java.io.IOException;
+import java.util.Scanner;
 
-/**
- * IMPORTANT:
- *      O nome da classe deve ser "Main" para que a sua solução execute
- *      Class name must be "Main" for your solution to execute
- *      El nombre de la clase debe ser "Main" para que su solución ejecutar
- */
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        /**
-         * Escreva a sua solução aqui
-         * Code your solution here
-         * Escriba su solución aquí
-         */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+        for (int i = 1; i <= n; i++) {
+            if (n%i==0){
+                System.out.println(i);
+            }
+        }
 
     }
 

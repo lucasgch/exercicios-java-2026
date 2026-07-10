00cdc16689eb3bc2c23b0f725dd3c65ddfeff00e
Lucas <Lucasgodoyjor@gmail.com>
2026-07-10 09:16:51 -0300

    Adiciona ex. 1153 - fatorial simples

--- beecrowd/iniciante/p4/p1153_fatorial_simples/Main.java
+++ beecrowd/iniciante/p4/p1153_fatorial_simples/Main.java
@@ -1,22 +1,21 @@
 package beecrowd.iniciante.p4.p1153_fatorial_simples;
 
 import java.io.IOException;
+import java.util.Scanner;
 
-/**
- * IMPORTANT:
- *      O nome da classe deve ser "Main" para que a sua solução execute
- *      Class name must be "Main" for your solution to execute
- *      El nombre de la clase debe ser "Main" para que su solución ejecutar
- */
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        /**
-         * Escreva a sua solução aqui
-         * Code your solution here
-         * Escriba su solución aquí
-         */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+
+        int fatorial = 1;
+        for (int i = 1; i <= n; i++) {
+            fatorial *= i;
+        }
+        System.out.println(fatorial);
 
     }
 

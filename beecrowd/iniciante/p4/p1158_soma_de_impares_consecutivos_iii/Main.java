4e4cc9e932d011058c606170282e2fc462e18c75
Lucas <Lucasgodoyjor@gmail.com>
2026-07-10 14:05:07 -0300

    Adiciona ex. 1058

--- beecrowd/iniciante/p4/p1158_soma_de_impares_consecutivos_iii/Main.java
+++ beecrowd/iniciante/p4/p1158_soma_de_impares_consecutivos_iii/Main.java
@@ -1,23 +1,32 @@
 package beecrowd.iniciante.p4.p1158_soma_de_impares_consecutivos_iii;
 
 import java.io.IOException;
+import java.util.Scanner;
 
-/**
- * IMPORTANT:
- *      O nome da classe deve ser "Main" para que a sua solução execute
- *      Class name must be "Main" for your solution to execute
- *      El nombre de la clase debe ser "Main" para que su solución ejecutar
- */
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        /**
-         * Escreva a sua solução aqui
-         * Code your solution here
-         * Escriba su solución aquí
-         */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
 
+        for (int i = 1; i <= n; i++) {
+
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            int soma = 0;
+            int impares = 0;
+
+            for (int j = x; impares < y; j++) {
+                if (j % 2 != 0) {
+                    soma += j;
+                    impares++;
+                }
+            }
+
+            System.out.println(soma);
+        }
+
+        sc.close();
     }
-
 }

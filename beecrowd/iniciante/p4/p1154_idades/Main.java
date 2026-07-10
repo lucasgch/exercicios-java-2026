305c3c231034b0edaba3a8bf21662f284ec2065d
Lucas <Lucasgodoyjor@gmail.com>
2026-07-10 09:25:16 -0300

    Adiciona ex. 1054 - idades

--- beecrowd/iniciante/p4/p1154_idades/Main.java
+++ beecrowd/iniciante/p4/p1154_idades/Main.java
@@ -1,22 +1,24 @@
 package beecrowd.iniciante.p4.p1154_idades;
 
 import java.io.IOException;
+import java.util.Scanner;
 
-/**
- * IMPORTANT:
- *      O nome da classe deve ser "Main" para que a sua solução execute
- *      Class name must be "Main" for your solution to execute
- *      El nombre de la clase debe ser "Main" para que su solución ejecutar
- */
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        /**
-         * Escreva a sua solução aqui
-         * Code your solution here
-         * Escriba su solución aquí
-         */
+        Scanner sc = new Scanner(System.in);
+
+        double media = 0;
+        int idade = sc.nextInt();
+        int contador = 0;
+        while(idade >= 0){
+            media+=idade;
+            idade = sc.nextInt();
+            contador++;
+        }
+        media = media/contador;
+        System.out.printf("%.2f%n",media);
 
     }
 

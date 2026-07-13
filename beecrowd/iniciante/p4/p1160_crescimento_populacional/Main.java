bb7bbb8a5e3d17518a3cc601557ef294e637e643
Lucas <Lucasgodoyjor@gmail.com>
2026-07-13 09:33:45 -0300

    Adiciona ex. 1160 - crescimento populacional

--- beecrowd/iniciante/p4/p1160_crescimento_populacional/Main.java
+++ beecrowd/iniciante/p4/p1160_crescimento_populacional/Main.java
@@ -1,23 +1,41 @@
 package beecrowd.iniciante.p4.p1160_crescimento_populacional;
 
 import java.io.IOException;
+import java.util.Scanner;
 
-/**
- * IMPORTANT:
- *      O nome da classe deve ser "Main" para que a sua solução execute
- *      Class name must be "Main" for your solution to execute
- *      El nombre de la clase debe ser "Main" para que su solución ejecutar
- */
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        /**
-         * Escreva a sua solução aqui
-         * Code your solution here
-         * Escriba su solución aquí
-         */
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
 
+        for (int i = 1; i <= t; i++) {
+            int PA = sc.nextInt();
+            int PB = sc.nextInt();
+            double G1 = sc.nextDouble();
+            double G2 = sc.nextDouble();
+
+            int anos = 0;
+
+            while (PA <= PB) {
+
+                PA += (int) (PA * (G1 / 100));
+                PB += (int) (PB * (G2 / 100));
+                anos++;
+
+                if (anos > 100) {
+                    break;
+                }
+            }
+
+            if (anos > 100) {
+                System.out.println("Mais de 1 seculo.");
+            } else {
+                System.out.println(anos + " anos.");
+            }
+        }
+
+        sc.close();
     }
-
 }

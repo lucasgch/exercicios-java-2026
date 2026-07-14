6416ee3b40e68ac977a3d46792b72200fff71f0e
Lucas <Lucasgodoyjor@gmail.com>
2026-07-14 13:35:49 -0300

    Adiciona ex. 1172 - Substituição em Vetor I

--- beecrowd/iniciante/p4/p1172_substituicao_em_vetor_i/Main.java
+++ beecrowd/iniciante/p4/p1172_substituicao_em_vetor_i/Main.java
@@ -1,22 +1,25 @@
 package beecrowd.iniciante.p4.p1172_substituicao_em_vetor_i;
 
 import java.io.IOException;
+import java.util.Scanner;
 
-/**
- * IMPORTANT:
- *      O nome da classe deve ser "Main" para que a sua solução execute
- *      Class name must be "Main" for your solution to execute
- *      El nombre de la clase debe ser "Main" para que su solución ejecutar
- */
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        /**
-         * Escreva a sua solução aqui
-         * Code your solution here
-         * Escriba su solución aquí
-         */
+        Scanner sc = new Scanner(System.in);
+        int[] x = new int[10];
+
+        for (int i = 0; i<x.length; i++){
+            x[i] = sc.nextInt();
+            if (x[i] ==0 || x[i]<0){
+                x[i] = 1;
+            }
+        }
+
+        for (int i = 0; i<x.length; i++){
+            System.out.printf("X[%d] = %d%n",i,x[i]);
+        }
 
     }
 

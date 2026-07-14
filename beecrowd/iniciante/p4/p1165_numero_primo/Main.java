520beb89f51eb299ac0937a5f4eb5963ed33b8e0
Lucas <Lucasgodoyjor@gmail.com>
2026-07-14 13:26:17 -0300

    adiciona ex. 1165 número primo

--- beecrowd/iniciante/p4/p1165_numero_primo/Main.java
+++ beecrowd/iniciante/p4/p1165_numero_primo/Main.java
@@ -2,21 +2,32 @@ package beecrowd.iniciante.p4.p1165_numero_primo;
 
 import java.io.IOException;
 
-/**
- * IMPORTANT:
- *      O nome da classe deve ser "Main" para que a sua solução execute
- *      Class name must be "Main" for your solution to execute
- *      El nombre de la clase debe ser "Main" para que su solución ejecutar
- */
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        /**
-         * Escreva a sua solução aqui
-         * Code your solution here
-         * Escriba su solución aquí
-         */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+
+            boolean isPrimo = true;
+            int x = sc.nextInt();
+            for (int j = 2; j <= x-1; j++) {
+
+                if (x % j == 0) {
+                    isPrimo = false;
+                    break;
+                }
+            }
+            System.out.println(isPrimo ? x + " eh primo" : x + " nao eh primo");
+        }
+
+        sc.close();
+
 
     }
 

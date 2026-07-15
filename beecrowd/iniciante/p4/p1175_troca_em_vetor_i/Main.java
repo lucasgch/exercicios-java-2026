18e147ac4b2bd35360058e0e6cd385cee7209366
Lucas <Lucasgodoyjor@gmail.com>
2026-07-15 09:31:27 -0300

    Adiciona ex. 1175 Troca em Vetor I

--- beecrowd/iniciante/p4/p1175_troca_em_vetor_i/Main.java
+++ beecrowd/iniciante/p4/p1175_troca_em_vetor_i/Main.java
@@ -1,23 +1,30 @@
 package beecrowd.iniciante.p4.p1175_troca_em_vetor_i;
 
 import java.io.IOException;
+import java.util.Scanner;
 
-/**
- * IMPORTANT:
- *      O nome da classe deve ser "Main" para que a sua solução execute
- *      Class name must be "Main" for your solution to execute
- *      El nombre de la clase debe ser "Main" para que su solución ejecutar
- */
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        /**
-         * Escreva a sua solução aqui
-         * Code your solution here
-         * Escriba su solución aquí
-         */
+        int[] n = new int[20];
+        Scanner sc = new Scanner(System.in);
 
+        // Leitura
+        for (int i = 0; i < n.length; i++) {
+            n[i] = sc.nextInt();
+        }
+
+        // Troca
+        int[] nt = new int[20];
+        for (int i = 0; i < n.length; i++) {
+            nt[n.length-i-1]=n[i];
+        }
+
+        // Impressão
+        for (int i = 0; i < nt.length; i++) {
+            System.out.printf("N[%d] = %d%n",i,nt[i]);
+        }
     }
 
 }

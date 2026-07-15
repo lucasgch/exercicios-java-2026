f744a79cc6b9bd659a24064f957607a354083174
Lucas <Lucasgodoyjor@gmail.com>
2026-07-15 09:23:11 -0300

    Adiciona ex. 1174 seleçao em Vetor I

--- beecrowd/iniciante/p4/p1174_selecao_em_vetor_i/Main.java
+++ beecrowd/iniciante/p4/p1174_selecao_em_vetor_i/Main.java
@@ -1,22 +1,25 @@
 package beecrowd.iniciante.p4.p1174_selecao_em_vetor_i;
 
 import java.io.IOException;
+import java.util.Scanner;
 
-/**
- * IMPORTANT:
- *      O nome da classe deve ser "Main" para que a sua solução execute
- *      Class name must be "Main" for your solution to execute
- *      El nombre de la clase debe ser "Main" para que su solución ejecutar
- */
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        /**
-         * Escreva a sua solução aqui
-         * Code your solution here
-         * Escriba su solución aquí
-         */
+        double[] vetor = new double[1];
+        Scanner input = new Scanner(System.in);
+        for (int i = 0; i < vetor.length; i++) {
+            vetor[i] = input.nextDouble();
+        }
+        input.close();
+
+        for  (int i = 0; i < vetor.length; i++) {
+            if (vetor[i] <= 10) {
+                System.out.printf("A[%d] = %.1f%n",i,vetor[i]);
+            }
+        }
+
 
     }
 

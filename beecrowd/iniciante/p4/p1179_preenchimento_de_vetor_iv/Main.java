471856ac81fa6e40c46e3ffefe623c43557ed69a
Lucas <Lucasgodoyjor@gmail.com>
2026-07-16 09:47:02 -0300

    adiciona ex. 1179 preenchimento de vetor iv

--- beecrowd/iniciante/p4/p1179_preenchimento_de_vetor_iv/Main.java
+++ beecrowd/iniciante/p4/p1179_preenchimento_de_vetor_iv/Main.java
@@ -1,23 +1,54 @@
 package beecrowd.iniciante.p4.p1179_preenchimento_de_vetor_iv;
 
 import java.io.IOException;
+import java.util.Scanner;
 
-/**
- * IMPORTANT:
- *      O nome da classe deve ser "Main" para que a sua solução execute
- *      Class name must be "Main" for your solution to execute
- *      El nombre de la clase debe ser "Main" para que su solución ejecutar
- */
 public class Main {
 
     public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
 
-        /**
-         * Escreva a sua solução aqui
-         * Code your solution here
-         * Escriba su solución aquí
-         */
+        int[] pares = new int[5];
+        int[] impares = new int[5];
 
+        int qtdPares = 0;
+        int qtdImpares = 0;
+
+        for (int i = 0; i < 15; i++) {
+            int x = sc.nextInt();
+
+            if (x % 2 == 0) {
+                pares[qtdPares] = x;
+                qtdPares++;
+
+                if (qtdPares == 5) {
+                    imprimeVetor(pares, qtdPares, "par");
+                    qtdPares = 0; // Reseta o contador para reutilizar o vetor
+                }
+            } else {
+                impares[qtdImpares] = x;
+                qtdImpares++;
+
+                if (qtdImpares == 5) {
+                    imprimeVetor(impares, qtdImpares, "impar");
+                    qtdImpares = 0; // Reseta o contador para reutilizar o vetor
+                }
+            }
+        }
+
+        if (qtdImpares > 0) {
+            imprimeVetor(impares, qtdImpares, "impar");
+        }
+        if (qtdPares > 0) {
+            imprimeVetor(pares, qtdPares, "par");
+        }
+
+        sc.close();
     }
 
+    private static void imprimeVetor(int[] vetor, int quantidade, String nomeVetor) {
+        for (int i = 0; i < quantidade; i++) {
+            System.out.println(nomeVetor + "[" + i + "] = " + vetor[i]);
+        }
+    }
 }

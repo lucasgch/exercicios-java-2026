c403d2d638ef21d1be5fbaab42484fccbd93e766
Lucas <Lucasgodoyjor@gmail.com>
2026-07-16 11:53:01 -0300

    Adiciona pasta 5 de exercícios

---
+++ beecrowd/iniciante/p5/p1189_area_esquerda/Main.java
@@ -0,0 +1,23 @@
+package beecrowd.iniciante.p5.p1189_a�rea_esquerda;
+
+import java.io.IOException;
+
+/**
+ * IMPORTANT:
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        /**
+         * Escreva a sua solução aqui
+         * Code your solution here
+         * Escriba su solución aquí
+         */
+
+    }
+
+}

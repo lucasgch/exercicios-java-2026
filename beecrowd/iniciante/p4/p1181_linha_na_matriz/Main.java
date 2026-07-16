555d4a45f85d9533874f65258f817ca31e8c85cb
Lucas <Lucasgodoyjor@gmail.com>
2026-07-16 11:42:30 -0300

    adiciona ex. 1181 - Linha na Matriz

--- beecrowd/iniciante/p4/p1181_linha_na_matriz/Main.java
+++ beecrowd/iniciante/p4/p1181_linha_na_matriz/Main.java
@@ -1,23 +1,38 @@
 package beecrowd.iniciante.p4.p1181_linha_na_matriz;
 
 import java.io.IOException;
+import java.util.Scanner;
+import java.util.Locale;
 
-/**
- * IMPORTANT:
- *      O nome da classe deve ser "Main" para que a sua solução execute
- *      Class name must be "Main" for your solution to execute
- *      El nombre de la clase debe ser "Main" para que su solución ejecutar
- */
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        /**
-         * Escreva a sua solução aqui
-         * Code your solution here
-         * Escriba su solución aquí
-         */
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        double soma=0;
 
+        int linhaDaOpASerRealizada = sc.nextInt();
+        char operacao = sc.next().charAt(0);
+
+
+        double[][] matriz = new double[12][12];
+        for (int i = 0; i < 12; i++) {
+            for (int j = 0; j < 12; j++) {
+                matriz[i][j] = sc.nextDouble();
+                if (i==linhaDaOpASerRealizada) {
+                    soma+=matriz[i][j];
+                }
+            }
+        }
+
+        if (operacao=='S') {
+            System.out.printf("%.1f%n",soma);
+        } else {
+            System.out.printf("%.1f%n",(soma/12.0));
+        }
+
+        sc.close();
     }
 
 }

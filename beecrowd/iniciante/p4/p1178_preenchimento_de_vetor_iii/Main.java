7d9bbec7ebe0a46fc19635b98e5bb65ef01f313e
Lucas <Lucasgodoyjor@gmail.com>
2026-07-16 09:30:06 -0300

    Adiciona ex. 1178 preenchimento de vetor iii

--- beecrowd/iniciante/p4/p1178_preenchimento_de_vetor_iii/Main.java
+++ beecrowd/iniciante/p4/p1178_preenchimento_de_vetor_iii/Main.java
@@ -1,23 +1,28 @@
 package beecrowd.iniciante.p4.p1178_preenchimento_de_vetor_iii;
 
 import java.io.IOException;
+import java.util.Scanner;
 
-/**
- * IMPORTANT:
- *      O nome da classe deve ser "Main" para que a sua solução execute
- *      Class name must be "Main" for your solution to execute
- *      El nombre de la clase debe ser "Main" para que su solución ejecutar
- */
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        /**
-         * Escreva a sua solução aqui
-         * Code your solution here
-         * Escriba su solución aquí
-         */
+        Scanner sc = new Scanner(System.in);
+        double[] x = new double[100];
 
+        x[0] = sc.nextDouble();
+        sc.close();
+
+        for  (int i = 1; i < x.length; i++) {
+            x[i] = x[i-1]/2;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i <= x.length -1; i++) {
+            sb.append("N[").append(i).append("] = ").append(String.format("%.4f", x[i])).append("\n");
+        }
+
+        System.out.print(sb.toString());
     }
 
 }

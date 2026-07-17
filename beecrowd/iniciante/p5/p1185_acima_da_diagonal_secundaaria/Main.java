510a21216228ded03bc251aaf47a737724026a85
Lucas <Lucasgodoyjor@gmail.com>
2026-07-17 11:36:51 -0300

    Adiciona ex.

--- beecrowd/iniciante/p5/p1185_acima_da_diagonal_secundaaria/Main.java
+++ beecrowd/iniciante/p5/p1185_acima_da_diagonal_secundaaria/Main.java
@@ -1,22 +1,37 @@
 package beecrowd.iniciante.p5.p1185_acima_da_diagonal_secundaaria;
 
 import java.io.IOException;
+import java.util.Scanner;
 
-/**
- * IMPORTANT:
- *      O nome da classe deve ser "Main" para que a sua solução execute
- *      Class name must be "Main" for your solution to execute
- *      El nombre de la clase debe ser "Main" para que su solución ejecutar
- */
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        /**
-         * Escreva a sua solução aqui
-         * Code your solution here
-         * Escriba su solución aquí
-         */
+        Scanner sc = new  Scanner(System.in);
+        char o = sc.next().charAt(0);
+
+        double[][] matriz = new double[12][12];
+
+        double soma = 0.0;
+        int contador = 0;
+
+        for (int i=0; i<12; i++){
+            for  (int j=0; j<12; j++){
+                matriz[i][j] = sc.nextDouble();
+
+                if (i+j < 11) {
+                    soma += matriz[i][j];
+                    contador++;
+                }
+            }
+        }
+        sc.close();
+
+        if (o == 'S') {
+            System.out.printf("%.1f%n", soma);
+        } else {
+            System.out.printf("%.1f%n", soma / contador);
+        }
 
     }
 

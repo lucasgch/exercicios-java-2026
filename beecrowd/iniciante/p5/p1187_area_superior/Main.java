db06b3d326d4eb7afffcfcc76cf32bc5f12237ff
Lucas <Lucasgodoyjor@gmail.com>
2026-07-17 11:56:03 -0300

    Adiciona ex. 1187 - Área Superior

---
+++ beecrowd/iniciante/p5/p1187_area_superior/Main.java
@@ -0,0 +1,39 @@
+package beecrowd.iniciante.p5.p1187_area_superior;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new  Scanner(System.in);
+        char o = sc.next().charAt(0);
+
+        double[][] matriz = new double[12][12];
+
+        double soma = 0.0;
+        int contador = 0;
+
+        for (int i=0; i<12; i++){
+            for  (int j=0; j<12; j++){
+                matriz[i][j] = sc.nextDouble();
+
+                if (j > i && j < 11 - i) {
+                    soma += matriz[i][j];
+                    contador++;
+                }
+
+            }
+        }
+        sc.close();
+
+        if (o == 'S') {
+            System.out.printf("%.1f%n", soma);
+        } else {
+            System.out.printf("%.1f%n", soma / contador);
+        }
+
+    }
+
+}

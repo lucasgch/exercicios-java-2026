552b6164a5bcc8649abc876ce503bcb7a54cd7d5
Lucas <Lucasgodoyjor@gmail.com>
2026-07-24 11:41:08 -0300

    Adiciona ex. 1534 matriz 123

--- beecrowd/iniciante/p5/p1534_matriz_123/Main.java
+++ beecrowd/iniciante/p5/p1534_matriz_123/Main.java
@@ -1,23 +1,49 @@
 package beecrowd.iniciante.p5.p1534_matriz_123;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 /**
- * IMPORTANT:
- *      O nome da classe deve ser "Main" para que a sua solução execute
- *      Class name must be "Main" for your solution to execute
- *      El nombre de la clase debe ser "Main" para que su solución ejecutar
- */
+
+* Prioridade 2: Esta posição está na diagonal secundária? Se sim, coloque 2.
+* Padrão: Se não for nenhuma das diagonais, coloque 3.
+*/
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        /**
-         * Escreva a sua solução aqui
-         * Code your solution here
-         * Escriba su solución aquí
-         */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
 
+        while (n>=3 && n<=70){
+
+            imprimeMatriz(n);
+
+            n = sc.nextInt();
+        }
+
+    }
+
+    private static void imprimeMatriz(int n) {
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                // Se diagonal principal: 1.
+                if (i==j){
+                    System.out.print(1);
+                }
+                // Se diagonal secundária: 2.
+                else if (i + j == n - 1){
+                    System.out.print(2);
+                }
+                // Senão: 3.
+                else {
+                    System.out.print(3);
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 
 }

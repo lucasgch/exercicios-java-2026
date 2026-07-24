b1389b4643eee09fa3c0fb0008f1ac53b603d423
Lucas <Lucasgodoyjor@gmail.com>
2026-07-24 14:10:03 -0300

    Adiciona ex. 1541

--- beecrowd/iniciante/p5/p1541_construindo_casas/Main.java
+++ beecrowd/iniciante/p5/p1541_construindo_casas/Main.java
@@ -1,23 +1,36 @@
 package beecrowd.iniciante.p5.p1541_construindo_casas;
 
 import java.io.IOException;
+import java.util.Scanner;
 
-/**
- * IMPORTANT:
- *      O nome da classe deve ser "Main" para que a sua solução execute
- *      Class name must be "Main" for your solution to execute
- *      El nombre de la clase debe ser "Main" para que su solución ejecutar
- */
+import static java.lang.Math.sqrt;
+
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        /**
-         * Escreva a sua solução aqui
-         * Code your solution here
-         * Escriba su solución aquí
-         */
+        Scanner sc = new Scanner(System.in);
 
+        while (sc.hasNextInt()) {
+
+            int a = sc.nextInt();
+
+            if (a == 0) {
+                break;
+            }
+
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+
+            int areaCasa = a * b;
+
+            int areaTerreno = (areaCasa * 100) / c;
+
+            int lado = (int) sqrt(areaTerreno);
+
+            System.out.println(lado);
+        }
+
+        sc.close();
     }
-
 }

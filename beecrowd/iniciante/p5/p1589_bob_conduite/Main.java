6cda80643577c163dfd996f3672ab1ddf997be65
Lucas <Lucasgodoyjor@gmail.com>
2026-07-27 14:38:51 -0300

    Adiciona ex. 1589 - Bob Conduite

--- beecrowd/iniciante/p5/p1589_bob_conduite/Main.java
+++ beecrowd/iniciante/p5/p1589_bob_conduite/Main.java
@@ -1,6 +1,7 @@
 package beecrowd.iniciante.p5.p1589_bob_conduite;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 /**
  * IMPORTANT:
@@ -12,11 +13,14 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        /**
-         * Escreva a sua solução aqui
-         * Code your solution here
-         * Escriba su solución aquí
-         */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            int r1 = sc.nextInt();
+            int r2 = sc.nextInt();
+            System.out.println(r1 + r2);
+        }
 
     }
 

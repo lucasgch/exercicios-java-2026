00ca66d0727e9d5260b9c2f6c907e09424db8d96
Lucas <Lucasgodoyjor@gmail.com>
2026-07-27 14:50:51 -0300

    Adiciona ex. 1759 - Ho Ho Ho

--- beecrowd/iniciante/p5/p1759_ho_ho_ho/Main.java
+++ beecrowd/iniciante/p5/p1759_ho_ho_ho/Main.java
@@ -1,22 +1,19 @@
 package beecrowd.iniciante.p5.p1759_ho_ho_ho;
 
 import java.io.IOException;
+import java.util.Scanner;
 
-/**
- * IMPORTANT:
- *      O nome da classe deve ser "Main" para que a sua solução execute
- *      Class name must be "Main" for your solution to execute
- *      El nombre de la clase debe ser "Main" para que su solución ejecutar
- */
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        /**
-         * Escreva a sua solução aqui
-         * Code your solution here
-         * Escriba su solución aquí
-         */
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        for (int i = 1; i < n; i++) {
+            System.out.print("Ho ");
+        }
+        System.out.println("Ho!");
 
     }
 

67b2b3340864ce701252ab7a6f48cdc75399658c
Lucas <Lucasgodoyjor@gmail.com>
2026-07-27 14:29:30 -0300

    Adiciona ex. 1564

--- beecrowd/iniciante/p5/p1564_vai_ter_copa/Main.java
+++ beecrowd/iniciante/p5/p1564_vai_ter_copa/Main.java
@@ -1,22 +1,25 @@
 package beecrowd.iniciante.p5.p1564_vai_ter_copa;
 
 import java.io.IOException;
+import java.util.Scanner;
 
-/**
- * IMPORTANT:
- *      O nome da classe deve ser "Main" para que a sua solução execute
- *      Class name must be "Main" for your solution to execute
- *      El nombre de la clase debe ser "Main" para que su solución ejecutar
- */
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        /**
-         * Escreva a sua solução aqui
-         * Code your solution here
-         * Escriba su solución aquí
-         */
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNext()) {
+            int n = scanner.nextInt();
+
+            if (n == 0) {
+                System.out.println("vai ter copa!");
+            } else {
+                System.out.println("vai ter duas!");
+            }
+        }
+
+        scanner.close();
 
     }
 

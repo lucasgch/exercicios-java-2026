25f801bea26952cc304db3d6272b353b08ea54f0
Lucas <Lucasgodoyjor@gmail.com>
2026-07-27 14:23:22 -0300

    Adiciona Matriz Quadrada III

--- beecrowd/iniciante/p5/p1557_matriz_quadrada_iii/Main.java
+++ beecrowd/iniciante/p5/p1557_matriz_quadrada_iii/Main.java
@@ -1,23 +1,56 @@
 package beecrowd.iniciante.p5.p1557_matriz_quadrada_iii;
 
 import java.io.IOException;
+import java.util.Scanner;
 
-/**
- * IMPORTANT:
- *      O nome da classe deve ser "Main" para que a sua solução execute
- *      Class name must be "Main" for your solution to execute
- *      El nombre de la clase debe ser "Main" para que su solución ejecutar
- */
 public class Main {
 
     public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
 
-        /**
-         * Escreva a sua solução aqui
-         * Code your solution here
-         * Escriba su solución aquí
-         */
+        while (sc.hasNextInt()) {
+            int n = sc.nextInt();
 
+            if (n == 0) {
+                break;
+            }
+
+            int[][] matriz = new int[n][n];
+
+            // Potências de 2 (2^(i+j))
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    matriz[i][j] = (int) Math.pow(2, i + j);
+                }
+            }
+
+            imprimeMatriz(matriz);
+            System.out.println(); // Linha em branco entre os casos de teste
+        }
+
+        sc.close();
     }
 
+    public static void imprimeMatriz(int[][] matriz) {
+        int n = matriz.length;
+
+        // O maior elemento da matriz fica na última posição [n-1][n-1]
+        int maiorValor = matriz[n - 1][n - 1];
+
+        // Calcula a quantidade de dígitos T do maior valor
+        int t = String.valueOf(maiorValor).length();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j == 0) {
+                    // Primeiro elemento da linha (sem espaço na frente)
+                    System.out.printf("%" + t + "d", matriz[i][j]);
+                } else {
+                    // Demais elementos com um espaço antes
+                    System.out.printf(" %" + t + "d", matriz[i][j]);
+                }
+            }
+            System.out.println();
+        }
+    }
 }

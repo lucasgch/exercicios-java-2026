1c360cd4f15fbeda9de3f9c1ab49f099cef7715a
Lucas <Lucasgodoyjor@gmail.com>
2026-08-10 11:59:54 -0300

    Adiciona ex. 1837 prefacio

--- beecrowd/iniciante/p5/p1837_prefacio/Main.java
+++ beecrowd/iniciante/p5/p1837_prefacio/Main.java
@@ -1,22 +1,24 @@
 package beecrowd.iniciante.p5.p1837_prefacio;
 
 import java.io.IOException;
+import java.util.Scanner;
 
-/**
- * IMPORTANT:
- *      O nome da classe deve ser "Main" para que a sua solução execute
- *      Class name must be "Main" for your solution to execute
- *      El nombre de la clase debe ser "Main" para que su solución ejecutar
- */
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        /**
-         * Escreva a sua solução aqui
-         * Code your solution here
-         * Escriba su solución aquí
-         */
+        Scanner sc = new Scanner(System.in);
+        int a =  sc.nextInt();
+        int b = sc.nextInt();
+        int resto = a % b;
+
+        // Segundo o enunciado o resto sempre deve ser >0
+        if (resto < 0) {
+            resto += Math.abs(b);
+        }
+
+        int q = (a - resto)/b;
+        System.out.printf("%d %d%n", q, resto);
 
     }
 

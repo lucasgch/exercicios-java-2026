f48d968d3e8e905c9f58b5915653ee09d6751a80
Lucas <Lucasgodoyjor@gmail.com>
2026-08-11 10:32:30 -0300

    1848 - Corvo Contador

--- beecrowd/iniciante/p5/p1848_corvocontador/Main.java
+++ beecrowd/iniciante/p5/p1848_corvocontador/Main.java
@@ -1,23 +1,28 @@
 package beecrowd.iniciante.p5.p1848_corvocontador;
 
 import java.io.IOException;
+import java.util.Scanner;
 
-/**
- * IMPORTANT:
- *      O nome da classe deve ser "Main" para que a sua solução execute
- *      Class name must be "Main" for your solution to execute
- *      El nombre de la clase debe ser "Main" para que su solución ejecutar
- */
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        /**
-         * Escreva a sua solução aqui
-         * Code your solution here
-         * Escriba su solución aquí
-         */
+        Scanner sc = new Scanner(System.in);
 
+        int cawTree = 1;
+        int soma = 0;
+        while (cawTree <= 3 && sc.hasNextLine()) {
+            String entrada = sc.nextLine().trim();
+            if (entrada.equals("caw caw")) {
+                System.out.println(soma);
+                soma = 0; // zera para o próximo número
+                cawTree++;
+            } else {
+                String binario = entrada.replace('*', '1').replace('-', '0');
+                soma += Integer.parseInt(binario, 2);
+
+            }
+        }
+        sc.close();
     }
-
 }

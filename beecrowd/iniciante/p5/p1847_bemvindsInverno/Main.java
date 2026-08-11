120d0126763073e77c34e9482834754a278f1c4a
Lucas <Lucasgodoyjor@gmail.com>
2026-08-11 10:05:20 -0300

    Adiciona 1847 Bem-vindos e Bem-vindas ao Inverno

--- beecrowd/iniciante/p5/p1847_bemvindsInverno/Main.java
+++ beecrowd/iniciante/p5/p1847_bemvindsInverno/Main.java
@@ -1,23 +1,54 @@
 package beecrowd.iniciante.p5.p1847_bemvindsInverno;
 
 import java.io.IOException;
+import java.util.Scanner;
 
-/**
- * IMPORTANT:
- *      O nome da classe deve ser "Main" para que a sua solução execute
- *      Class name must be "Main" for your solution to execute
- *      El nombre de la clase debe ser "Main" para que su solución ejecutar
- */
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        /**
-         * Escreva a sua solução aqui
-         * Code your solution here
-         * Escriba su solución aquí
-         */
+        Scanner sc = new Scanner(System.in);
+        int a =  sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        sc.close();
 
+        //Se a temperatura permaneceu constante do 1º para o 2º dia.
+        if (a == b ) {
+            // As pessoas ficam felizes se subiu do 2º para o 3º dia
+            if (c>b){
+                System.out.println(":)");
+            // ou tristes caso contrário (respectivamente, sétima e oitava figuras)
+            } else {
+                System.out.println(":(");
+            }
+        // Se a temperatura subiu do 1º para o 2º dia
+        } else if (b>a){
+            // e subiu do 3º para o 2º
+            if (c>b) {
+                // mas subiu do 2º para o 3º menos do que subira do 1º para o 2º
+                if (c-b<b-a) {
+                    System.out.println(":(");
+                } else {
+                    System.out.println(":)");
+                }
+            // mas desceu ou permaneceu constante do 2º para o 3º, as pessoas ficam tristes (segunda figura)
+            } else {
+                System.out.println(":(");
+            }
+        // Se a temperatura desceu do 1º para o 2º dia
+        } else {
+            // mas subiu ou permaneceu constante do 2º para o 3º
+            if (c>=b){
+                System.out.println(":)");
+            } else {
+                if (b-c<a-b){
+                    System.out.println(":)");
+                } else {
+                    System.out.println(":(");
+                }
+            }
+        }
     }
 
 }

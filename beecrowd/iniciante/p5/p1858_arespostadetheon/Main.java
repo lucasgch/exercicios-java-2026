ce9999150dff5e96a95ab7cc2348b427fb84a1de
Lucas <Lucasgodoyjor@gmail.com>
2026-08-11 13:13:28 -0300

    1858 - A Resposta de Theon

--- beecrowd/iniciante/p5/p1858_arespostadetheon/Main.java
+++ beecrowd/iniciante/p5/p1858_arespostadetheon/Main.java
@@ -1,23 +1,29 @@
 package beecrowd.iniciante.p5.p1858_arespostadetheon;
 
 import java.io.IOException;
+import java.util.Scanner;
 
-/**
- * IMPORTANT:
- *      O nome da classe deve ser "Main" para que a sua solução execute
- *      Class name must be "Main" for your solution to execute
- *      El nombre de la clase debe ser "Main" para que su solución ejecutar
- */
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        /**
-         * Escreva a sua solução aqui
-         * Code your solution here
-         * Escriba su solución aquí
-         */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
 
+        int minGolpes = Integer.MAX_VALUE;
+        int pessoaEscolhida = 1;
+
+        for (int i = 1; i <= n; i++) {
+            int golpes = sc.nextInt();
+
+            // O operador strict `<` garante o desempate pela menor posição
+            if (golpes < minGolpes) {
+                minGolpes = golpes;
+                pessoaEscolhida = i;
+            }
+        }
+
+        System.out.println(pessoaEscolhida);
+        sc.close();
     }
-
 }

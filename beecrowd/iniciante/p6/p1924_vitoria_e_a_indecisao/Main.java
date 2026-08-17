cf50070b34b8db5a4fd9b316d7f3e4b34c83ec77
Lucas <Lucasgodoyjor@gmail.com>
2026-08-17 13:06:31 -0300

    1924 - Vitória e a Indecisão

--- beecrowd/iniciante/p6/p1924_vitoria_e_a_indecisao/Main.java
+++ beecrowd/iniciante/p6/p1924_vitoria_e_a_indecisao/Main.java
@@ -1,22 +1,19 @@
 package beecrowd.iniciante.p6.p1924_vitoria_e_a_indecisao;
 
 import java.io.IOException;
+import java.util.Scanner;
 
-/**
- * IMPORTANT:
- *      O nome da classe deve ser "Main" para que a sua solução execute
- *      Class name must be "Main" for your solution to execute
- *      El nombre de la clase debe ser "Main" para que su solución ejecutar
- */
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        /**
-         * Escreva a sua solução aqui
-         * Code your solution here
-         * Escriba su solución aquí
-         */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            sc.nextLine();
+        }
+        System.out.println("Ciencia da Computacao");
 
     }
 

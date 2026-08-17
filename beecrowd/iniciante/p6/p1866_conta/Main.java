ed13cc9988db6ae5ebd3280192396a4151d8da52
Lucas <Lucasgodoyjor@gmail.com>
2026-08-17 11:52:10 -0300

    Adiciona ex. 1866 conta

--- beecrowd/iniciante/p6/p1866_conta/Main.java
+++ beecrowd/iniciante/p6/p1866_conta/Main.java
@@ -1,23 +1,28 @@
 package beecrowd.iniciante.p6.p1866_conta;
 
 import java.io.IOException;
+import java.util.Scanner;
 
-/**
- * IMPORTANT:
- *      O nome da classe deve ser "Main" para que a sua solução execute
- *      Class name must be "Main" for your solution to execute
- *      El nombre de la clase debe ser "Main" para que su solución ejecutar
- */
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        /**
-         * Escreva a sua solução aqui
-         * Code your solution here
-         * Escriba su solución aquí
-         */
-
+        Scanner sc = new Scanner(System.in);
+        int c = sc.nextInt();
+        sc.close();
+        int soma = 0;
+        for (int i = 1; i <= c; i++) {
+            int termos = sc.nextInt();
+            for (int j = 1; j <= termos; j++) {
+                if (j % 2 != 0) {
+                    soma++;
+                } else {
+                    soma--;
+                }
+            }
+            System.out.println(soma);
+            soma = 0;
+        }
     }
 
 }

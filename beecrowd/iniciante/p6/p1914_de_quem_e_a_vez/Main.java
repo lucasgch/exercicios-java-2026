f1671b3fafe572c88cebf3492c42838b9faa052a
Lucas <Lucasgodoyjor@gmail.com>
2026-08-17 12:10:37 -0300

    Adiciona ex. 1914 de quem e a vez

--- beecrowd/iniciante/p6/p1914_de_quem_e_a_vez/Main.java
+++ beecrowd/iniciante/p6/p1914_de_quem_e_a_vez/Main.java
@@ -1,23 +1,42 @@
 package beecrowd.iniciante.p6.p1914_de_quem_e_a_vez;
 
 import java.io.IOException;
+import java.util.Scanner;
 
-/**
- * IMPORTANT:
- *      O nome da classe deve ser "Main" para que a sua solução execute
- *      Class name must be "Main" for your solution to execute
- *      El nombre de la clase debe ser "Main" para que su solución ejecutar
- */
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        /**
-         * Escreva a sua solução aqui
-         * Code your solution here
-         * Escriba su solución aquí
-         */
+        Scanner sc = new Scanner(System.in);
+        int qt = sc.nextInt();
 
+        for (int i = 0; i < qt; i++) {
+            String jogador1 = sc.next();
+            String jogada1 = sc.next();
+            String jogador2 = sc.next();
+            String jogada2 = sc.next();
+
+            long num1 = sc.nextLong();
+            long num2 = sc.nextLong();
+            long soma = num1 + num2;
+
+            boolean somaEhPar = (soma % 2 == 0);
+
+            if (somaEhPar) {
+                if (jogada1.equals("PAR")) {
+                    System.out.println(jogador1);
+                } else {
+                    System.out.println(jogador2);
+                }
+            } else {
+                if (jogada1.equals("IMPAR")) {
+                    System.out.println(jogador1);
+                } else {
+                    System.out.println(jogador2);
+                }
+            }
+        }
+
+        sc.close();
     }
-
 }

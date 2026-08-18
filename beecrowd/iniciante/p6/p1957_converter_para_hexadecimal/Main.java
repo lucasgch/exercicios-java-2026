1a826622d7f0c1cafaf5c0ac2947ba918ac94664
Lucas <Lucasgodoyjor@gmail.com>
2026-08-18 13:33:29 -0300

    Adiciona ex. 1957 - Converte de decimal para hexadecimal

--- beecrowd/iniciante/p6/p1957_converter_para_hexadecimal/Main.java
+++ beecrowd/iniciante/p6/p1957_converter_para_hexadecimal/Main.java
@@ -1,22 +1,38 @@
 package beecrowd.iniciante.p6.p1957_converter_para_hexadecimal;
 
 import java.io.IOException;
+import java.math.BigInteger;
+import java.util.Scanner;
 
-/**
- * IMPORTANT:
- *      O nome da classe deve ser "Main" para que a sua solução execute
- *      Class name must be "Main" for your solution to execute
- *      El nombre de la clase debe ser "Main" para que su solución ejecutar
- */
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        /**
-         * Escreva a sua solução aqui
-         * Code your solution here
-         * Escriba su solución aquí
-         */
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        sc.close();
+        long resto = 0;
+        StringBuilder hex = new StringBuilder();
+
+        do {
+            resto = n%16;
+            n = n/16;
+            if (resto<=9){
+                hex.append(resto);
+            } else {
+                char resultado = switch ((int) resto){
+                    case 10 -> 'A';
+                    case 11 -> 'B';
+                    case 12 -> 'C';
+                    case 13 -> 'D';
+                    case 14 -> 'E';
+                    case 15 -> 'F';
+                    default -> throw new IllegalStateException("Unexpected value: " + resto);
+                };
+                hex.append(resultado);
+            }
+        } while (n>0);
+        System.out.println(hex.reverse());
 
     }
 

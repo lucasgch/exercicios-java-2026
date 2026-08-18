2a24d90bc9d6620aeb9fbf72b94060ac897d77c8
Lucas <Lucasgodoyjor@gmail.com>
2026-08-18 13:08:38 -0300

    Adiciona ex. 1933 tri du

--- beecrowd/iniciante/p6/p1933_tri_du/Main.java
+++ beecrowd/iniciante/p6/p1933_tri_du/Main.java
@@ -1,23 +1,28 @@
 package beecrowd.iniciante.p6.p1933_tri_du;
 
 import java.io.IOException;
+import java.util.Scanner;
 
-/**
- * IMPORTANT:
- *      O nome da classe deve ser "Main" para que a sua solução execute
- *      Class name must be "Main" for your solution to execute
- *      El nombre de la clase debe ser "Main" para que su solución ejecutar
- */
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
         /**
-         * Escreva a sua solução aqui
-         * Code your solution here
-         * Escriba su solución aquí
+         * Um trio (três cartas de mesmo valor) ganha de uma dupla (duas cartas de mesmo valor).
+         * Um trio formado por cartas de maior valor ganha de um trio formado por cartas de menor valor.
+         * Uma dupla formada por cartas de maior valor ganha de uma dupla formada por cartas de menor valor.
          */
-
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        sc.close();
+        if (a==b){
+            System.out.println(a);
+        } else if (a>b){
+            System.out.println(a);
+        } else {
+            System.out.println(b);
+        }
     }
 
 }
